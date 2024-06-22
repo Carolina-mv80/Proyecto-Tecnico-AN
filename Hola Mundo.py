83b33ed32ee436e81e82d9e7bd658d4e9ffbeff9
Carolina-mv80 <carolinamendoza.instructor@gmail.com>
2024-06-21 21:12:07 -0500

    Subiendo archivos

---
+++ Hola Mundo.py
@@ -0,0 +1,31 @@
+print ("HOLA MUNDO - HELLO WORLD")
+print ('HOLA MUNDO - HELLO WORLD')
+print ('''HOLA MUNDO - HELLO WORLD''')
+'''
+ESTA ES LA FORMA DE DEFINIR UN COMENTARIO
+PARA VARAS LÌNEAS '''    '''
+X=3
+print ("El valor asignado es: ",X)
+
+x="Carolina"
+print ("El nombre guardado es: ",x)
+print ("El valor asignado es: ",X)
+'''
+
+'''
+print ("FORMA 1")
+print ("Digite palabra o frase: ")
+texto = input()
+print (" La palabra o frase digitada es: ", texto)
+'''
+'''
+print ("FORMA 2")
+texto = input("Digite palabra o frase: ")
+print (" La palabra o frase digitada es: ", texto)
+'''
+
+num1 = int(input("Digite un nùmero entero mayor que 0: "))
+num2 = int(input("Digite un nùmero entero mayor que 0: "))
+suma= num1+num2
+print (" El resultado de la suma es:", suma)
+
---
+++ Practica 1.py
@@ -0,0 +1,23 @@
+print ('Hola Mundo')
+nombre="Andrès"
+salario=800000
+
+print("su nombre es: ",nombre)
+print("su salario es: ",salario)
+
+
+print("FORMA 1")
+print("Digite un texto o palabra")
+nombre = input()
+print ("el texto digitado es: ", nombre)
+
+print("Digite un salario")
+salario =input()
+print ("el salario digitado es: ", salario)
+
+print("FORMA 2 ")
+nombre = input("Digite un nombre: ")
+print ("el texto digitado es: ", nombre)
+
+salario =input("Digite un salario: ")
+print ("el salario digitado es: ", salario)
